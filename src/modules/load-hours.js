c1bb761b943bb01a0177a1dcb0ba1859917efa7e
Israel Teixeira <israeltx@gmail.com>
2025-06-25 17:23:01 -0300

    Check hours availability and render available hours as options inside the select element

--- src/modules/load-hours.js
+++ src/modules/load-hours.js
@@ -12,7 +12,20 @@ const actualHour = dayjs(new Date()).format('H')
 
 
 function loadHours() {
+  let unavailableHours = []
+  let schedule_hours = []
+
+  // Check hour availability
   available_hours.forEach((hour) => {
+    if (hour < actualHour) {
+      unavailableHours.push(hour)
+    } else {
+      schedule_hours.push(hour)
+    }
+  })
+
+  // Render available hours as options inside select
+  schedule_hours.forEach((hour) => {
     // Create the option
     const option = document.createElement('option')
     // Set the option's value to an hour
