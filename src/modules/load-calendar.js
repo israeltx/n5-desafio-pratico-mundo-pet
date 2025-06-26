7563095b00a2f7ec1bccb05fc03aa8e94d7204f1
Israel Teixeira <israeltx@gmail.com>
2025-06-26 11:40:50 -0300

    Set the load-calendar.js to the search-date input; Adjust search-date's margin-top

--- src/modules/load-calendar.js
+++ src/modules/load-calendar.js
@@ -3,10 +3,13 @@ import dayjs from "dayjs"
 
 // Date input
 const date_input = document.getElementById('date')
+const search_date = document.getElementById('search-date')
 
 // Get today's date
 const todaysDate = dayjs(new Date()).format('YYYY-MM-DD')
-// Load the actual date on the date input
+// Load the actual date on the date inputs
 date_input.value = todaysDate
+search_date.value = todaysDate
 // Prevent the user from selecting a date before today
 date_input.min = todaysDate
+search_date.min = todaysDate
